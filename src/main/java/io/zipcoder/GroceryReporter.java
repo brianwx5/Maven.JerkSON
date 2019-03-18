ce986d098ef7b21390c058b11c53c006b963b960
Brian Wong <brianwong@zipcoders-MacBook-Pro.local>
2019-03-18 18:52:03 -0400

    Finished

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,16 +1,65 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GroceryReporter {
     private final String originalFileText;
+    ItemParser itemParser = new ItemParser();
+    HashMap<String, ArrayList<Item>> itemHashMap = new HashMap<>();
 
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
 
+
+    public HashMap<String, ArrayList<Item>> itemToHashMap(String originalFileText) {
+        List<Item> itemList = itemParser.parseItemList(originalFileText);
+        for (Item item : itemList) {
+            ArrayList<Item> temp = itemHashMap.getOrDefault(item.getName(), new ArrayList<>());
+            temp.add(item);
+            itemHashMap.put(item.getName(), temp);
+        }
+        return itemHashMap;
+    }
+
+    public HashMap<Double, Integer> priceAndCount(ArrayList<Item> arrayListOfItems) {
+        HashMap<Double, Integer> priceCountHashMap = new HashMap<>();
+        for (Item item : arrayListOfItems) {
+            Integer count = priceCountHashMap.getOrDefault(item.getPrice(),0);
+            priceCountHashMap.put(item.getPrice(), count + 1);
+        }
+        return priceCountHashMap;
+    }
+
+
     @Override
     public String toString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        this.itemHashMap = itemToHashMap(originalFileText);
+
+        for (Map.Entry<String, ArrayList<Item>> entry : itemHashMap.entrySet()) {
+            sb.append("name:    " + entry.getKey() + "        seen:" + entry.getValue().size() + " times\n"
+                    + "=============        =============\n\n");
+
+            ArrayList<Item> subArray = entry.getValue();
+            HashMap<Double, Integer> priceAndCount = priceAndCount(subArray);
+            for (Map.Entry<Double, Integer> price : priceAndCount.entrySet()) {
+                sb.append("price:   " + price.getKey() + "         seen:" + price.getValue() + " times\n"
+                        + "-------------        -------------\n");
+            }
+
+        }
+
+        sb.append("Errors                    Seen:" + itemParser.getNumberOfErrors() +" times\n");
+        return sb.toString();
     }
+
 }
+
+
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,82 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
+
+    public String regexForKeyValue = "[!|:|@|\\^|\\*|\\%|;]";
+    public int numberOfErrors = 0;
+
+    public String patternForSingleItem =
+              "naMe" + regexForKeyValue + "(\\w*)" +regexForKeyValue
+            + "price" + regexForKeyValue + "(\\d*.?\\d*)" + regexForKeyValue
+            + "type"  + regexForKeyValue + "(\\w*)"       + regexForKeyValue
+            + "expiration" + regexForKeyValue + "(\\d{1,2}/\\d{1,2}/\\d{2,4})#";
+
+
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        ArrayList<Item> itemList = new ArrayList<>();
+        String[] itemByLine = valueToParse.split("(?<=#)#");
+        for (String s: itemByLine) {
+            try {
+                itemList.add(parseSingleItem(s));
+            } catch (ItemParseException e) {
+                e.printStackTrace();
+                numberOfErrors++;
+
+            }
+        } return itemList;
     }
 
+
+
+
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        singleItem = singleItem.replaceAll("(?<=[a-zA-Z])0(?=[a-zA-Z])","o");
+        Item item = null;
+        try{
+        Pattern pattern = Pattern.compile(patternForSingleItem,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(singleItem);
+
+        matcher.find();
+        String name = matcher.group(1).toLowerCase();
+        if(name.equals("")) {
+            throw new ItemParseException();
+        }
+        String price = matcher.group(2);
+            if(price.equals("")) {
+                throw new ItemParseException();
+            }
+        String type = matcher.group(3).toLowerCase();
+            if(type.equals("")) {
+                throw new ItemParseException();
+            }
+        String expiration = matcher.group(4);
+            if(expiration.equals("")) {
+                throw new ItemParseException();
+            }
+
+        Double priceToDouble = Double.parseDouble(price);
+
+
+        item = new Item(name,priceToDouble,type,expiration);}
+
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new ItemParseException();
+        }
+
+        return item;
+
+
+    }
+
+    public int getNumberOfErrors() {
+        return numberOfErrors;
     }
 }
